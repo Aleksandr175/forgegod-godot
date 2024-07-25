a8bda25ce39d675691d923cbbb48b6bd696b3cfc
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2024-07-26 00:05:37 +0200

    Change score

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
-@onready var animation_player = $AnimationPlayer
+@onready var animation_coin = $AnimationCoin
 
 func _on_body_entered(body):
 	if body is Player:	
-		game_manager.add_point()
-		animation_player.play("pickup")
+		game_manager.add_point(1)
+		animation_coin.play("pickup")
+
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,14 +1,16 @@
 extends Node
 
-var score = 0
+signal gained_coins(int)
+var coins = 0
 
 @onready var score_label = $ScoreLabel
 var player: Player
 var current_checkpoint: Checkpoint
 
-func add_point():
-	score += 1
-	score_label.text = "You collected " + str(score) + " coins."
+func add_point(gained_coins):
+	coins += 1
+	emit_signal("gained_coins", gained_coins)
+	score_label.text = "You collected " + str(coins) + " coins."
 
 func respawn_player():
 	if current_checkpoint != null:
