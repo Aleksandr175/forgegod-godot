b6f2756f2f27221cb485677e1ae87ad1ed89efda
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2024-07-28 00:26:08 +0200

    Add changing scenes, add house, add level for house

--- scenes/levels/game.gd
+++ scenes/levels/game.gd
@@ -9,14 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-
-func _on_door_area_2d_2_body_entered(body):
-	print("door")
-	pass # Replace with function body.
-
-
-func _on_roof_area_2d_body_entered(body):
-	print("roof")
-	pass # Replace with function body.
---
+++ scenes/players/player_house.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+@onready var animated_sprite = $AnimatedSpritePlayer
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	var direction_vertical = Input.get_axis("move_up", "move_down")
+
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction_vertical:
+		velocity.y = direction_vertical * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	update_animations(direction, direction_vertical)
+	move_and_slide()
+	
+	
+func update_animations(direction: int, direction_vertical: int):
+	if direction == 0:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("run")
+
+	# Flip the Sprite
+	# TODO: improve animations, it turns right if we goes down-left
+	if direction > 0 or direction_vertical > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0 or direction_vertical < 0:
+		animated_sprite.flip_h = true
