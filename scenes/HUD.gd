71944f385ff4d1398b133975b4ca8a2d1b8afdd1
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2024-08-04 14:25:26 +0200

    Add pause menu

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -1,8 +1,26 @@
 extends CanvasLayer
 
 func _ready():
+	GameManager.pause_menu = $PauseMenu
+	# TODO: menu is opened, trigger play_pause twice to solve problem, why?
+	GameManager.play_pause()
+	GameManager.play_pause()
+
 	GameManager.gained_coins.connect(update_coin_display)
 	update_coin_display(GameManager.coins)
+
+func _process(_delta):
+	if Input.is_action_just_pressed("Open menu"):
+		GameManager.play_pause()
+		get_tree().paused = GameManager.isPaused
 	
 func update_coin_display(gained_coins):
 	$CoinLabel.text = str(GameManager.coins)
+
+
+func _on_resume_pressed():
+	print('resume')
+	GameManager.resume()
+
+func _on_exit_pressed():
+	GameManager.exit()
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,16 +2,32 @@ extends Node
 
 signal gained_coins(int)
 var coins: int = 0
+var isPaused = false
 
 @onready var score_label = $ScoreLabel
 var player: Player
 var current_checkpoint: Checkpoint
+var pause_menu
 
 func add_point(gained_coins: int):
 	coins += gained_coins
 	emit_signal("gained_coins", gained_coins)
 	#score_label.text = "You collected " + str(coins) + " coins."
 
+func play_pause():
+	isPaused = !isPaused
+	pause_menu.visible = isPaused
+	get_tree().paused = isPaused
+
 func respawn_player():
 	if current_checkpoint != null:
 		player.position = current_checkpoint.global_position
+
+func resume():
+	play_pause()
+
+func restart():
+	get_tree().reload_current_scene()
+		
+func exit():
+	get_tree().quit()
