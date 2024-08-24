459284ce4859d672f4c469e4173d934fbea8495b
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2024-08-24 23:49:06 +0200

    Add shader for resources and refactor script for resources

--- Entities/objects/resources/resource.gd
+++ Entities/objects/resources/resource.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var resourceDictionaryId: String
 var increase_qty = 3
 var item = null
+@onready var sprite_2d = $Sprite2D
 
 func die():
 	item = Inventory.inventory_dictionary[resourceDictionaryId]
@@ -16,5 +17,9 @@ func die():
 	queue_free()
 
 func get_damage():
-	# TODO: play animation
-	pass
+	var tween = get_tree().create_tween()
+	tween.tween_method(SetShader_BlinkIntensity, 1.0, 0.0, 0.3)
+
+func SetShader_BlinkIntensity(newValue: float):
+	sprite_2d.material.set_shader_parameter("blink_intensity", newValue)
+	
