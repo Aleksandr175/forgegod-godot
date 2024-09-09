16b5dd3218dc6a46fa12ea82708ba26d535d884c
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2024-09-09 13:54:25 +0200

    Add torch with light for cave level, change cave level

--- Stages/Levels/level-4.gd
+++ Stages/Levels/level-4.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var player = $Player
+@onready var levels_menu_ui = $LevelsMenuUI
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +14,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_cave_exit_level_menu_opened():
+	levels_menu_ui.visible = !levels_menu_ui.visible
+	get_tree().paused = !get_tree().paused
